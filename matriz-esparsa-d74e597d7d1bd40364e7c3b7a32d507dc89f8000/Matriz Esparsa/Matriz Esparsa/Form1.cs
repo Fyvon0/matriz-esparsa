53adedc751e123051b6ee079797b2e37eb28d0f5
lucasAlmeida272 <almeidadelucas@gmail.com>
2017-04-11 21:47:10 -0300

    Desenvolvido método MultiplicarMatrizes
    oi

---
+++ matriz-esparsa-d74e597d7d1bd40364e7c3b7a32d507dc89f8000/Matriz Esparsa/Matriz Esparsa/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matriz_Esparsa
+{
+    public partial class Form1 : Form
+    {
+        ListaLigadaCruzada matriz1,matriz2,matriz3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(cbxGrids.SelectedItem))
+            {
+                case 1:
+                    try
+                    {
+                        if (!matriz1.InserirEm(Convert.ToInt32(nudLinhas.Value), Convert.ToInt32(nudColunas.Value), Convert.ToDouble(nudValor.Value)))
+                            MessageBox.Show("Já existe um valor nessa posição");
+                        else
+                            matriz1.ExibirNoGridView(dataGridView1);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    break;
+                case 2:
+                    try
+                    {
+                        if (!matriz2.InserirEm(Convert.ToInt32(nudLinhas.Value), Convert.ToInt32(nudColunas.Value), Convert.ToDouble(nudValor.Value)))
+                            MessageBox.Show("Já existe um valor nessa posição");
+                        else
+                            matriz2.ExibirNoGridView(dataGridView2);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(cbxGrids.SelectedItem))
+            {
+                case 1:
+                    nudValor.Value = Convert.ToDecimal(matriz1.ValorDe(Convert.ToInt32(nudLinhas.Value), Convert.ToInt32(nudColunas.Value)));
+                    break;
+                case 2:
+                    nudValor.Value = Convert.ToDecimal(matriz2.ValorDe(Convert.ToInt32(nudLinhas.Value), Convert.ToInt32(nudColunas.Value)));
+                    break;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(cbxGrids.SelectedItem))
+            {
+                case 1:
+                    matriz1.RemoverElemento(Convert.ToInt32(nudLinhas.Value), Convert.ToInt32(nudColunas.Value));
+                    matriz1.ExibirNoGridView(dataGridView1);
+                    break;
+                case 2:
+                    matriz2.RemoverElemento(Convert.ToInt32(nudLinhas.Value), Convert.ToInt32(nudColunas.Value));
+                    matriz2.ExibirNoGridView(dataGridView2);
+                    break;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(cbxGrids.SelectedItem))
+            {
+                case 1:
+                    matriz1.SomaValorEmColuna(Convert.ToDouble(nudValor.Value), Convert.ToInt32(nudColunas.Value));
+                    matriz1.ExibirNoGridView(dataGridView1);
+                    break;
+                case 2:
+                    matriz2.SomaValorEmColuna(Convert.ToInt32(nudValor.Value), Convert.ToInt32(nudColunas.Value));
+                    matriz2.ExibirNoGridView(dataGridView2);
+                    break;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(cbxGrids.SelectedItem))
+            {
+                case 1:
+                    matriz1.Limpar();
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Columns.Clear();
+                    cbxGrids.Items.Remove(cbxGrids.SelectedItem);
+                    if (cbxGrids.Items.Count == 0)
+                        gbxOperacoes.Enabled = false;
+                    else
+                      cbxGrids.SelectedIndex = 0;
+                    break;
+                case 2:
+                    matriz2.Limpar();
+                    dataGridView2.Rows.Clear();
+                    dataGridView2.Columns.Clear();
+                    cbxGrids.Items.Remove(cbxGrids.SelectedItem);
+                    if (cbxGrids.Items.Count == 0)
+                        gbxOperacoes.Enabled = false;
+                    else
+                       cbxGrids.SelectedIndex = 0;
+                    break;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                return;
+
+            switch (Convert.ToInt32(nudCriarGridView.Value))
+            {
+                case 1:
+                    matriz1.LerArquivo(openFileDialog1.FileName);
+                    matriz1.ExibirNoGridView(dataGridView1);
+                    break;
+                case 2:
+                    matriz2.LerArquivo(openFileDialog1.FileName);
+                    matriz2.ExibirNoGridView(dataGridView2);
+                    break;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (cbxGrids.Items.Count == 2)
+                ListaLigadaCruzada.MultiplicarMatrizes(matriz1, matriz2).ExibirNoGridView(dataGridView3);
+            else
+                MessageBox.Show("É necessário ter duas matrizes para fazer a multiplicação");      
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (cbxGrids.Items.Count == 2)
+                ListaLigadaCruzada.SomarMatrizes(matriz1, matriz2).ExibirNoGridView(dataGridView3);
+            else
+                MessageBox.Show("É necessário ter duas matrizes para fazer a soma");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (Convert.ToInt32(nudCriarGridView.Value))
+            {
+                case 1:
+                    matriz1 = new ListaLigadaCruzada(Convert.ToInt32(nudCriarLinhas.Value), Convert.ToInt32(nudCriarColunas.Value));
+                    matriz1.ExibirNoGridView(dataGridView1);
+                    if (!cbxGrids.Items.Contains(1))
+                        cbxGrids.Items.Add(1);
+                    break;
+                case 2:
+                    matriz2 = new ListaLigadaCruzada(Convert.ToInt32(nudCriarLinhas.Value), Convert.ToInt32(nudCriarColunas.Value));
+                    matriz2.ExibirNoGridView(dataGridView2);
+                    if (!cbxGrids.Items.Contains(2))
+                        cbxGrids.Items.Add(2);
+                    break;
+            }
+            cbxGrids.SelectedIndex = 0;
+
+            gbxOperacoes.Enabled = true;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ matriz-esparsa-d74e597d7d1bd40364e7c3b7a32d507dc89f8000/Matriz Esparsa/Matriz Esparsa/ListaLigadaCruzada.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matriz_Esparsa
+{
+    class ListaLigadaCruzada
+    {
+        Celula primeiro;
+        Celula anterior;
+        Celula atual;
+        int linhas;
+        int colunas;
+
+        //Todos os métodos que não foram atualizados, precisam ser revistos e adaptados para
+
+        //Criar a estrutura básica da matriz esparsa com dimensão M x N - Construtor - feito - atualizado
+        //Inserir um novo elemento em uma posição(l, c) da matriz - InserirEm - feito
+        //Ler um arquivo texto com as coordenadas e os valores não nulos, armazenando-os na matriz - a fazer
+        //Retornar o valor de uma posição(l, c) da matriz - ValorDe - feito
+        //Exibir a matriz na tela, em um gridView - ExibirNoGridView - feito
+        //Liberar todas as posições alocadas da matriz, incluindo sua estrutura básica - Limpar - feito
+        //Remover o elemento(l, c) da matriz - RemoverElemento - feito
+        //Somar a constante K a todos os elementos da coluna c da matriz - SomaValorEmColuna - feito
+        //Somar duas matrizes esparsas, cada uma representada em uma estrutura própria e ambas exibidas
+        //em seu próprio gridView.O resultado deve gerar uma nova estrutura de matriz esparsa e exibido
+        //em um gridview próprio - a fazer
+        //Multiplicar duas matrizes esparsas, cada uma representada em uma estrutura própria e ambas
+        //exibidas em seu próprio gridView.O resultado deve gerar uma nova estrutura de matriz esparsa e
+        //exibido em um gridview próprio. - a fazer
+
+        //Criar a estrutura básica da matriz esparsa com dimensão M x N - Construtor - feito - atualizado
+        public ListaLigadaCruzada(int qtdeLinhas, int qtdeColunas)
+        {
+            qtdeLinhas++;   // A primeira linha é feita de nós cabeças, havendo l-1 linhas de dados
+            qtdeColunas++;  // O mesmo para as colunas
+            linhas = qtdeLinhas;
+            colunas = qtdeColunas;
+
+            primeiro = new Celula(null, null, 0, 0, Double.NaN);
+            int i = 1;
+            anterior = primeiro;
+            atual = null;
+            for (; i < qtdeLinhas; i++)
+            {
+                atual = new Celula(null, anterior, i, 0, Double.NaN);
+                anterior = atual;
+            }
+            primeiro.Abaixo = atual;
+
+
+            anterior = primeiro;
+            atual = null;
+            for (i = 1; i < qtdeColunas; i++)
+            {
+                atual = new Celula(anterior, null, 0, i, Double.NaN);
+                anterior = atual;
+            }
+            primeiro.Direita = atual;
+        }
+
+        //Inserir um novo elemento em uma posição(l, c) da matriz - InserirEm - feito
+        public bool InserirEm (int linha, int coluna, double valor)
+        {
+            if (linha > linhas.Length - 1 || linha < 1 || coluna > colunas.Length - 1 || coluna < 1)
+                return false;
+
+            linhaAnterior = linhas[linha];
+            linhaAtual = linhas[linha].Direita;
+            while (linhaAtual.Coluna <= coluna || linhaAtual.Coluna != 0)
+            {
+                if (linhaAtual.Coluna == coluna)
+                    return false;
+                linhaAnterior = linhaAtual;
+                linhaAtual = linhaAtual.Direita;
+            }
+
+            Celula novaCelula = new Celula(linhaAtual, null, linha, coluna, valor);
+            linhaAnterior.Direita = novaCelula;
+
+            colunaAnterior = colunas[coluna];
+            colunaAtual = colunas[coluna].Abaixo;
+            while (colunaAtual.Linha <= linha || colunaAtual.Linha != 0)
+            {
+                if (colunaAtual.Linha == linha)
+                    return false;
+                colunaAnterior = colunaAtual;
+                colunaAtual = colunaAtual.Abaixo;
+            }
+
+            colunaAnterior.Abaixo = novaCelula;
+            novaCelula.Abaixo = colunaAtual;
+
+            return true;
+        }
+
+        //Retornar o valor de uma posição(l, c) da matriz - ValorDe - feito
+        public double ValorDe (int linha, int coluna)
+        {
+            if (linha > linhas.Length - 1 || linha < 1 || coluna > colunas.Length - 1 || coluna < 1)
+                throw new Exception("Valor de linha e/ou coluna inválido");
+
+            linhaAtual = linhas[linha].Direita;
+            while (linhaAtual.Coluna <= coluna || linhaAtual.Coluna != 0)
+            {
+                if (linhaAtual.Coluna == coluna)
+                    return linhaAtual.Valor;
+                linhaAtual = linhaAtual.Direita;
+            }
+
+            return 0.0D;
+        }
+         
+        //Exibir a matriz na tela, em um gridView - ExibirNoGridView - feito
+        public void ExibirNoGridView(DataGridView dgv)
+        {
+            dgv.Rows.Clear();
+            dgv.Columns.Clear();
+            dgv.ColumnCount = colunas.Length - 2;
+            dgv.RowCount = linhas.Length - 2;
+
+            linhaAtual = linhas[1].Direita;
+            while (linhaAtual.Linha != 0)
+            {
+                while (linhaAtual.Coluna != 0)
+                    dgv.Rows[linhaAtual.Linha - 1].Cells[linhaAtual.Coluna - 1].Value = linhaAtual.Valor;
+                linhaAtual = linhaAtual.Abaixo.Direita;
+            }
+        }
+
+        //Liberar todas as posições alocadas da matriz, incluindo sua estrutura básica - Limpar - feito
+        public void Limpar()
+        {
+            int i;
+            for (i = 0; i < colunas.Length; i++)
+                colunas[i] = null;
+            for (i = 0; i < linhas.Length; i++)
+                linhas[i] = null;
+            linhaAnterior = linhaAtual = colunaAnterior = colunaAtual = null;
+        }
+
+        //Remover o elemento(l, c) da matriz - RemoverElemento - feito
+        public bool RemoverElemento (int linha, int coluna)
+        {
+            if (linha > linhas.Length - 1 || linha < 1 || coluna > colunas.Length - 1 || coluna < 1)
+                return false;
+
+            linhaAnterior = linhas[linha];
+            linhaAtual = linhas[linha].Direita;
+            bool achou = false;
+            while (linhaAtual.Coluna <= coluna || linhaAtual.Coluna != 0)
+            {
+                if (linhaAtual.Coluna == coluna)
+                {
+                    achou = true;
+                    break;
+                }
+                linhaAnterior = linhaAtual;
+                linhaAtual = linhaAtual.Direita;
+            }
+
+            if (!achou)
+                return false;
+
+            linhaAnterior.Direita = linhaAtual.Direita;
+
+            linhaAtual = null;
+
+            colunaAnterior = colunas[coluna];
+            colunaAtual = colunas[coluna].Abaixo;
+            while (colunaAtual.Linha < linha)
+            {
+                colunaAnterior = colunaAtual;
+                colunaAtual = colunaAtual.Abaixo;
+            }
+            colunaAnterior.Abaixo = colunaAtual.Abaixo;
+            colunaAtual = null;
+
+            return true;
+        }
+
+        //Somar a constante K a todos os elementos da coluna c da matriz - SomaValorEmColuna - feito
+        public void SomaValorEmColuna (double valor, int coluna)
+        {
+            if (coluna < 1 || coluna > colunas.Length - 1)
+                throw new Exception("Valor de coluna inválido");
+
+            if (Double.IsNaN(valor))
+                throw new Exception("Não é possível somar NaN a um valor");
+
+            colunaAtual = colunas[coluna].Abaixo;
+            while (colunaAtual.Linha != 0)
+            {
+                colunaAtual.Valor += valor;
+                colunaAtual = colunaAtual.Abaixo;
+            }
+        }
+    }
+}
